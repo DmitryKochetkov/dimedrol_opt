b07731eb24f8bc433c5904865e481eb84699f7f4
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-04-29 00:09:14 +0300

    Красивый main для лабораторных работ

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include "Swarm.h"
 #include "PointFormat.h"
 
-int main() {
+void particleSwarmOptimization() {
+    double optimalLowerBounds[3] = {-10.0, -5.12, -500.0};
+    double optimalUpperBounds[3] = {10.0, 5.12, 500.0};
+
     std::cout << "Выберите размерность аргумента функции: ";
     int dimension;
     std::cin >> dimension;
@@ -31,12 +34,12 @@ int main() {
             break;
 
         default:
-            return 0;
+            return;
     }
 
     std::cout << "Данная функция имеет минимум " << objective->getGlobalMinimum()
-    << " в точке " << pointToString(objective->getGlobalMinimumPoint()) << std::endl;
-    Swarm swarm(objective, 3000, -5.12, 5.12, 0.3, 2, 5);
+              << " в точке " << pointToString(objective->getGlobalMinimumPoint()) << std::endl;
+    Swarm swarm(objective, 3000, optimalLowerBounds[foo_code - 1], optimalUpperBounds[foo_code - 1], 0.3, 2, 5);
 
     std::cout << "Введите количество итераций: ";
     int iterations;
@@ -46,6 +49,41 @@ int main() {
         swarm.nextIteration();
 
     std::cout << "Найденный минимум: " << swarm.getGlobalBestMinimum()
-    << " в точке " << pointToString(swarm.getGlobalBestCoord());
+              << " в точке " << pointToString(swarm.getGlobalBestCoord());
+}
+
+void antColonyOptimization() {
+
+}
+
+void beeColonyOptimization() {
+
+}
+
+int main() {
+    int algorithm;
+    std::cout << "Выберите алгоритм: " << std::endl;
+    std::cout << "\t1. Алгоритм роя частиц" << std::endl;
+    std::cout << "\t2. Алгоритм муравьиной колонии" << std::endl;
+    std::cout << "\t3. Алгоритм пчелиной колонии" << std::endl;
+
+    std::cin >> algorithm;
+    switch (algorithm) {
+        case 1:
+            particleSwarmOptimization();
+            break;
+
+        case 2:
+            antColonyOptimization();
+            break;
+
+        case 3:
+            beeColonyOptimization();
+            break;
+
+        default:
+            return 0;
+    }
+
     return 0;
 }
